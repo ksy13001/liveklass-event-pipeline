2f3f2926fdcbfb4304e833e797e63f9996e9dc47
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-06-15 23:52:34 +0900

    feat: connect_db 분리 및 예외처리 추가

--- event_writer.py
+++ event_writer.py
@@ -1,50 +1,52 @@
 import mysql.connector
 from dotenv import load_dotenv
 import os
-load_dotenv()
+import logging
 
-DB_CONFIG = {
-    "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT")),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
-}
+load_dotenv()
 
 INSERT_QUERY = """
 INSERT INTO events (
-    event_id, 
-    event_type, 
-    user_id, 
-    order_id, 
-    lecture_id, 
-    amount, 
-    payment_method, 
+    event_id,
+    event_type,
+    user_id,
+    order_id,
+    lecture_id,
+    amount,
+    payment_method,
     timestamp)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """
 
-def write_event_to_db(event:dict):
-    conn = mysql.connector.connect(
-        host=DB_CONFIG["host"],
-        port=DB_CONFIG["port"],
-        user=DB_CONFIG["user"],
-        password=DB_CONFIG["password"],
-        database=DB_CONFIG["database"]
+
+def connect_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
     )
-    
-    cursor = conn.cursor()
-    cursor.execute(INSERT_QUERY, (
-        event["event_id"],
-        event["event_type"],
-        event["user_id"],
-        event["order_id"],
-        event["lecture_id"],
-        event["amount"],
-        event["payment_method"],
-        event["timestamp"]
-    ))
-    
-    conn.commit()
-    cursor.close()
-    conn.close()
+
+
+def write_event(conn, event: dict):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                INSERT_QUERY,
+                (
+                    event["event_id"],
+                    event["event_type"],
+                    event["user_id"],
+                    event["order_id"],
+                    event["lecture_id"],
+                    event["amount"],
+                    event["payment_method"],
+                    event["timestamp"],
+                ),
+            )
+        conn.commit()
+    except Exception as e:
+        logging.error(f"Error occurred while writing event: {e}")
+        conn.rollback()
+        raise
